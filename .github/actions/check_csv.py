5a909b82fe61ccc1a76e034eb9205c279fcc7ff7
ruotu55 <146649217+ruotu55@users.noreply.github.com>
2025-01-22 10:33:58 +0200

    Update check_csv.py

--- .github/actions/check_csv.py
+++ .github/actions/check_csv.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import sys
 
 # Path to the CSV file relative to the repository root
 csv_file = '.map-editor/locale/items.csv'
@@ -29,7 +30,7 @@ rows_passed = 0
 rows_failed = 0
 
 # Function to check the string lengths in each language column for each row
-def check_string_length():
+def check_string_length(event_prefix):
     global rows_checked, rows_passed, rows_failed
     with open(csv_file, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
@@ -39,6 +40,9 @@ def check_string_length():
                 continue
 
             prefix = row[0]  # The prefix like '22120_tulips_i_flower_pink_name'
+            if not prefix.startswith(event_prefix):
+                continue
+
             row_passed = True
             rows_checked += 1
 
@@ -64,8 +68,13 @@ def check_string_length():
     with open(os.environ['GITHUB_STEP_SUMMARY'], 'a') as summary_file:
         summary_file.write(f"## Summary of String Length Check\n")
         summary_file.write(f"- Total rows checked: {rows_checked}\n")
-        summary_file.write(f"- ✅ Rows passed: {rows_passed}\n")
-        summary_file.write(f"- ❌ Rows failed: {rows_failed}\n")
+        summary_file.write(f"- :white_check_mark: Rows passed: {rows_passed}\n")
+        summary_file.write(f"- :x: Rows failed: {rows_failed}\n")
 
 if __name__ == "__main__":
-    check_string_length()
+    if len(sys.argv) != 2:
+        print("Usage: python check_csv.py <event_prefix>")
+        sys.exit(1)
+
+    event_prefix = sys.argv[1]
+    check_string_length(event_prefix)
