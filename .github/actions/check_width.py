e3dd3ebf1d16bbd6295c58d8568fc372c8c219dd
ruotu55 <146649217+ruotu55@users.noreply.github.com>
2025-02-17 11:57:41 +0200

    Update check_width.py

--- .github/actions/check_width.py
+++ .github/actions/check_width.py
@@ -3,7 +3,7 @@ import sys
 import os
 from PIL import ImageFont, ImageDraw, Image
 
-def get_text_pixel_width(text, font_path='/usr/share/fonts/truetype/alegreya-sc/AlegreyaSC-Black.ttf', font_size=10):
+def get_text_pixel_width(text, font_path='/usr/share/fonts/truetype/alegreya-sc/AlegreyaSansSC-Black.ttf', font_size=10):
     # Use a truetype font
     font = ImageFont.truetype(font_path, font_size)
     # Create a dummy image and get the bounding box of the text
@@ -14,7 +14,7 @@ def get_text_pixel_width(text, font_path='/usr/share/fonts/truetype/alegreya-sc/
     return width
 
 def main(event_name):
-    font_path = '/usr/share/fonts/truetype/alegreya-sc/AlegreyaSC-Black.ttf'  # Path to the AlegreyaSC-Black truetype font file
+    font_path = '/usr/share/fonts/truetype/alegreya-sc/AlegreyaSansSC-Black.ttf'  # Path to the AlegreyaSC-Black truetype font file
     font_size = 10  # Font size
     try:
         words = event_name.split()
@@ -24,4 +24,6 @@ def main(event_name):
         total_width_including_spaces = get_text_pixel_width(event_name, font_path, font_size)
         # Prepare the summary text
         summary_text = f"The pixel width of the words and the total pixel width of the event name '{event_name}' including spaces is as follows:\n\n"
-        for word, width in word
+        for word, width in word_widths.items():
+            summary_text += f"The pixel width of the word '{word}' is: {width}\n"
+        summary_text += f"\nThe total pixel width of the event name '{event_name}' including spaces is: {total_width
